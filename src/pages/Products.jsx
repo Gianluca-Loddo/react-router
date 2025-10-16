f2e8281707e723c2f97d6129f9dbcf5ebd3616f8
Gianluca-Loddo <gianlu.loddo@gmail.com>
2025-10-16 14:24:28 +0200

    (products): axios fetch + debug log; (DefaultLayout): fixexd span

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,7 +1,23 @@
 import { useState, useEffect } from "react";
-
+import axios from "axios";
 
 export default function Products() {
+
+    //stato: caricamento lista dei prodotti 
+    const [products, setProducts] = useState([]); 
+    
+    // FETCHING DEI DATI (useState / AXIOS) - caricamento della lista prodotti
+    function fetchProducts() {
+        axios.get("https://fakestoreapi.com/products")
+            .then((res) => {
+                console.log(res.data); //debug
+                setProducts(res.data); //con axios i dati sono in res.data
+            });
+    }
+
+    // esegue il FETCHING una sola volta (al primo render)
+    useEffect(fetchProducts, []);
+
     return (
         <>
             <h2 className="mb-3">
